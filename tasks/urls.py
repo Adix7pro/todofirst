989e9c075bcc6509f7d148d2cae2a1a03506c220
adhamchoriyevuzb <138776040+adhamchoriyevuzb@users.noreply.github.com>
2023-07-21 02:24:07 +0500

    first edit

---
+++ tasks/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from . import views
+
+
+urlpatterns = [
+    path('', views.index,name="list"),
+    path('update_task/<str:pk>/', views.updateTask, name="update_task"),
+    path('deleted/<str:pk>/', views.deleteTask, name="deleted"),
+
+]
